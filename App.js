dc26c1d8c6f9e4380f798722a1148b597340141b
TerhiElina <terhi.evakallio@gmail.com>
2024-02-01 16:43:38 +0200

    Tehtävä valmis

--- App.js
+++ App.js
@@ -1,10 +1,50 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TextInput, View, FlatList, Button} from 'react-native';
+import { useState } from 'react';
 
 export default function App() {
+  const [item, setItem]= useState('');
+  const [shoppingList, setShoppingList] = useState([]);
+
+  const AddItem =() =>{
+    setShoppingList([...shoppingList,item])
+    setItem(''); //tyhjennetään inputField
+  }
+  const ClearList = () => {
+    setShoppingList([])
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
+    <View style={{marginTop: 70, marginBottom: 20}}> 
+      <TextInput
+      style={styles.input}
+      placeholder='insert item'
+      onChangeText = {text => setItem(text)}
+      value={item} // tässä asetetaan tekstikentän valueksi item, jotta tyhjennyt onnistuu
+      />
+    </View>
+    <View style={styles.buttonContainer}>
+      <Button 
+        title='Add'
+        onPress={AddItem}
+        />
+        <Button 
+        title='Clear'
+        onPress={ClearList}
+        />
+    </View>
+    <View style={{flex:5}}>
+      <Text style={{padding: 5}}>Shoppinglist:</Text>  
+      <FlatList
+        data={shoppingList} //Datan arvoksi asetetaan messages state
+        renderItem={({item}) => 
+          <View style ={styles.listItem}>
+            <Text>{item}</Text>
+          </View>
+      }
+      />
+    </View>
       <StatusBar style="auto" />
     </View>
   );
@@ -17,4 +57,15 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  input:{
+    borderColor:'grey',
+    borderWidth:1,
+    width: 250,
+  },
+  buttonContainer:{
+    flexDirection:'row',
+    marginBottom: 25,
+    justifyContent: 'space-around',
+    width: '30%',
+  }
 });
